a4960bb8f022539dcaf99227f8fe6b7748f3b282
Daniela Prado <danipra2050@gmail.com>
2024-11-28 13:59:37 -0500

    Ejercicios 1.1-1.2

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,14 @@
+import Part from "./Part";
+
+/* eslint-disable react/prop-types */
+const Content = (props) => {
+  return (
+    <>
+    <Part part={props.parts[0].part} exercises={props.parts[0].exercises} />
+    <Part part={props.parts[1].part} exercises={props.parts[1].exercises} />
+    <Part part={props.parts[2].part} exercises={props.parts[2].exercises} />
+    </>
+    
+  );
+}
+export default Content;
---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  );
+}
+export default Part;
